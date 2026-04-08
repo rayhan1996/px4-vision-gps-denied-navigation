0670efd522c78af42c082708e87b15b3859af8db
Rayhane Gholampoor <102473760+rayhan1996@users.noreply.github.com>
2026-04-08 20:34:53 +0330

    Add feature matching function using OpenCV

---
+++ vision_pipeline/odometry/feature_matcher.py
@@ -0,0 +1,7 @@
+import cv2
+
+def match_features(desc1, desc2):
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(desc1, desc2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    return matches
