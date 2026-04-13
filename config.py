d7fda580013ead88a4e920e655dd6d9e7b54773e
Rayhane Gholampoor <102473760+rayhan1996@users.noreply.github.com>
2026-04-13 21:17:04 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# =========================
+# Dataset Paths
+# =========================
+
+CAM0_PATH = "C:/Users/MRPC/Datasets/EuRoC/MH_01_easy/mav0/cam0/data"
+CAM1_PATH = "C:/Users/MRPC/Datasets/EuRoC/MH_01_easy/mav0/cam1/data"
+
+# =========================
+# Camera Intrinsics (EuRoC)
+# =========================
+
+K_EUROC = [
+    [458.654, 0, 367.215],
+    [0, 457.296, 248.375],
+    [0, 0, 1]
+]
